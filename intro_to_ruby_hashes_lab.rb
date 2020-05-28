245f54b9dfe6babb98f0af2ac7b23ad3d295a676
Roman Opalacz <roman9891@gmail.com>
2020-05-28 19:11:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,7 +26,7 @@ def monopoly_with_third_tier
   {
     railroads: {
         pieces: 4, 
-        rent_in_dollars: {one_piece_owned: 25},
+        rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50},
         names: {}
       }
   }
